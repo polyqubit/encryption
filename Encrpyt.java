e7584cdea4f9ab3904a787c5cbb1be4ca356b378
polyqubit <89955098+polyqubit@users.noreply.github.com>
2021-11-03 15:30:37 -0700

    integer encrypt

--- Encrpyt.java
+++ Encrpyt.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public final class Encrypt {
 	public static int[] toArray(int data, boolean print) {
 		int copy = data;
-		int write = 0
-		ArrayList<Integer> storageTemp = new Arraylist<>();
+		int write = 0;
+		ArrayList<Integer> storageTemp = new ArrayList<Integer>();
 		
 		while(copy!=0) {
 			write = copy%10;
@@ -15,14 +15,37 @@ public final class Encrypt {
 		int otherCounter = 0;
 		for(int i=storageTemp.size()-1; i>=0; i--) {
 			out[otherCounter] = storageTemp.get(i);
+      otherCounter++;
 		}
 		if(print) {
-			System.out.println("[ ");
-			for(int : )
+			System.out.print("[ ");
+			for(int i : out) {
+        System.out.print(i+" ");
+      }
+      System.out.println(" ]");
 		}
 		return out;
 	}
+  public static int toInt(int[] data) {
+    int placeCounter = 1;
+    int total = 0;
+    for(int i=data.length-1;i>=0;i--) {
+      total += data[i] * placeCounter;
+      placeCounter *= 10;
+    }
+    return total;
+  }
 	public static int rotateEnc(int data, int key) {
-		return 0;
+		int[] dataArray = toArray(data, false);
+    int test = 0;
+    for(int i=0; i<dataArray.length; i++) {
+      dataArray[i] += key;
+      if(dataArray[i]>9) {
+        test = dataArray[i]%10;
+        dataArray[i] = test;
+      }
+      System.out.println(dataArray[i]);
+    }
+    return toInt(dataArray);
 	}
 }
