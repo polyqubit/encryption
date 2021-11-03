439e3d48d6f519d261e142860ac990953024bca9
polyqubit <89955098+polyqubit@users.noreply.github.com>
2021-11-03 15:30:09 -0700

    add integer rotate

--- starter.java
+++ starter.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class starter {
+public class Main {
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int data = 235825332;
-		Encrypt.toArray(data, true);
+		int data = 123456;
+    		System.out.println(Encrypt.toInt(Encrypt.toArray(data, true)));
+	    	int encryptedData = Encrypt.rotateEnc(data,202);
+	    	System.out.println(encryptedData);
+	    	Encrypt.toArray(encryptedData, true);
+	    	//Method of encryption useless
+	    	//There are only 9 possible keys(10 including key = 0)
 	}
 }
